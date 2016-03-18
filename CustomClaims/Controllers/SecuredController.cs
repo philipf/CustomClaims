29159cbcb46443db96bde8e50ba2057fc6e6032d
Philip Fourie <philipf@agilebridge.co.za>
2016-03-18 11:08:35 +0200

    Initial commit

---
+++ CustomClaims/Controllers/SecuredController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Mvc;
+
+namespace CustomClaims.Controllers
+{
+    [Authorize]
+    public class SecuredController : Controller
+    {
+        // GET: Secured
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
