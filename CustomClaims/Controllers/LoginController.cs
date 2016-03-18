d44e5298346b3b27b9e4a7a072a137643c7a03cb
Philip Fourie <philipf@agilebridge.co.za>
2016-03-18 14:23:55 +0200

    Login shell

---
+++ CustomClaims/Controllers/LoginController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CustomClaims.Models;
+
+namespace CustomClaims.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Login
+        [HttpPost]
+        public ActionResult Index(LoginViewModel vm, string returnUrl)
+        {
+            if (vm.User == "admin" && vm.Password == "admin")
+            {
+                return Redirect(returnUrl);
+            }
+
+            return View();
+        }
+    }
+}
---
+++ CustomClaims/Models/LoginViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace CustomClaims.Models
+{
+    public class LoginViewModel
+    {
+        public string User { get; set; }
+        public string Password { get; set; }
+    }
+}
