2d2f91e47ae8f467edf90d88036d4b97f185098d
Philip Fourie <philipf@agilebridge.co.za>
2016-03-18 15:24:54 +0200

    Most basic identity login

--- CustomClaims/Controllers/LoginController.cs
+++ CustomClaims/Controllers/LoginController.cs
@@ -1,30 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
+using System.Security.Principal;
 using System.Web;
 using System.Web.Mvc;
 using CustomClaims.Models;
+using Microsoft.Owin.Security;
+using Microsoft.AspNet.Identity;
 
 namespace CustomClaims.Controllers
 {
     public class LoginController : Controller
     {
-        // GET: Login
         public ActionResult Index()
         {
             return View();
         }
 
-        // GET: Login
         [HttpPost]
         public ActionResult Index(LoginViewModel vm, string returnUrl)
         {
-            if (vm.User == "admin" && vm.Password == "admin")
-            {
-                return Redirect(returnUrl);
-            }
+            if (!(vm.User == "admin" && vm.Password == "admin"))
+                return View();
 
-            return View();
+            var claims = new List<Claim>();
+            claims.Add(new Claim(ClaimTypes.Name, vm.User));
+            var id = new ClaimsIdentity(claims, DefaultAuthenticationTypes.ApplicationCookie);
+
+            AuthenticationManager.SignIn(new AuthenticationProperties { IsPersistent = false }, id);
+
+            return Redirect(returnUrl);
+        }
+
+        private IAuthenticationManager AuthenticationManager
+        {
+            get { return HttpContext.GetOwinContext().Authentication; }
         }
     }
 }
